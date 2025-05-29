a6dfb292f717e823cfe97ab95a4b19fb10e8c85b
Bala Bharath Doppalapudi <106264369+balu9032@users.noreply.github.com>
2025-05-29 20:39:59 +0530

    put method added

--- src/main/java/com/example/expensemanager/ExpenseController.java
+++ src/main/java/com/example/expensemanager/ExpenseController.java
@@ -1,30 +1,54 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 package com.example.expensemanager;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(
+   origins = {"*"}
+)
 @RestController
-@RequestMapping("/api/expenses")
+@RequestMapping({"/api/expenses"})
 public class ExpenseController {
+   @Autowired
+   private ExpenseRepository expenseRepository;
 
-    @Autowired
-    private ExpenseRepository expenseRepository;
+   public ExpenseController() {
+   }
 
-    @GetMapping
-    public List<Expense> getAllExpenses() {
-        return expenseRepository.findAll();
-    }
+   @GetMapping
+   public List<Expense> getAllExpenses() {
+      return this.expenseRepository.findAll();
+   }
 
-    @PostMapping
-    public Expense createExpense(@RequestBody Expense expense) {
-        return expenseRepository.save(expense);
-    }
+   @PostMapping
+   public Expense createExpense(@RequestBody Expense expense) {
+      return (Expense)this.expenseRepository.save(expense);
+   }
 
-    @DeleteMapping("/{id}")
-    public void deleteExpense(@PathVariable Long id) {
-        expenseRepository.deleteById(id);
-    }
+   @PutMapping({"/{id}"})
+   public Expense updateExpense(@PathVariable Long id, @RequestBody Expense expenseDetails) {
+      Expense expense = (Expense)this.expenseRepository.findById(id).orElseThrow(() -> {
+         return new RuntimeException("Expense not found with id " + String.valueOf(id));
+      });
+      expense.setDesc(expenseDetails.getDesc());
+      expense.setAmount(expenseDetails.getAmount());
+      expense.setDate(expenseDetails.getDate());
+      expense.setAmountType(expenseDetails.getAmountType());
+      return (Expense)this.expenseRepository.save(expense);
+   }
+
+   @DeleteMapping({"/{id}"})
+   public void deleteExpense(@PathVariable Long id) {
+      this.expenseRepository.deleteById(id);
+   }
 }
